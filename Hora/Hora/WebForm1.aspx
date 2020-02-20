5f9b3b147d215b1e0cb0937e3e5b1c1af9e58ad5
¨Daniela <danielaalvarezvc@gmail.com>
2020-02-20 13:16:46 -0400

    subiendo los primeros archivos

---
+++ Hora/Hora/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Hora.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+    <style type="text/css">
+        html{
+            background-color:greenyellow;
+
+        }
+        body{
+            background-color: goldenrod;
+        }
+    </style>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="llamar" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Hora/Hora/WebService1.asmx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Hora
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://Ver_la_hora")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string Hora()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
